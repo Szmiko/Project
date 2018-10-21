6afb2e779ade36874429d056336419d9af506094
Szymon Grabowski <szmiko@wp.pl>
2018-10-21 20:30:41 +0200

    JQuery fadeIn fadeOut

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
 $(function() {
-	var span = $('div').find('span');
-	span.each(function(index, element) {
-		if(index % 2 == 0) {
-			$(element).css('color', 'red');
-		}
-	});
-	
-	var paragraph = $('p');
-	paragraph.each(function(index, element) {
-		var button = '<button class="btn" data-tmp="' + index + '">Click me!</button>'
-		$(element).append(button);
-	});
+	$('body').fadeIn(1200);
+	$('p').hide();
 
-	$('button').click(function(){
-		alert($(this).attr('data-tmp'));
+	$('button').click(function() {
+		var paragraph = $('p');
+		var isVisible = paragraph.is(":visible");
+
+		if(isVisible) {
+			paragraph.fadeOut("slow");
+		} else {
+			paragraph.fadeIn("fast");
+		}
 	});
 });
